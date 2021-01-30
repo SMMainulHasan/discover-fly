9dacddc217b5e71cb5d5c57e81e0b1304687a7d2
SMMainulHasan <mainulhasan7070@gmail.com>
2021-01-30 18:18:05 +0600

    Everithing done well

--- discover-fly.js
+++ discover-fly.js
@@ -10,23 +10,27 @@ function plusMinusTicketQuantity(booleanIncreaseQuantity, setType, booleanFirstC
     }
     ticketQuantity.value = ticketCount;
     totalCalculation(booleanFirstClass);
-}
+};
 
 //Brake Down Plus minus button function
 function totalCalculation(booleanFirstClass) {
     const subtotal = document.getElementById('subtotal');
+    //Need to access quantity
     const firstClassQuantity = document.getElementById('first-class-qty');
     const firstClassQuantityNum = parseInt(firstClassQuantity.value);
+    //Need to access quantity again
     const economyQuantity = document.getElementById('economy-qty');
     const economyQuantityNum = parseInt(economyQuantity.value);
+    //First class ticket price $150
     let subtotalAmountFirst = firstClassQuantityNum * 150;
+    //Economy ticket price $100
     let subtotalAmountEconomy = economyQuantityNum * 100;
     let subtotalCost = subtotalAmountFirst + subtotalAmountEconomy;
     if (booleanFirstClass == true) {
         subtotalAmountFirst = firstClassQuantityNum;
-    } if (booleanFirstClass == false) {
+    }; if (booleanFirstClass == false) {
         subtotalAmountEconomy = economyQuantityNum;
-    }
+    };
     subtotal.innerText = ('$' + subtotalCost);
 
     let vat = subtotalCost * 0.1;
@@ -34,20 +38,20 @@ function totalCalculation(booleanFirstClass) {
 
     let total = subtotalCost + vat;
     document.getElementById('total').innerText = ('$'+ total);
-}
+};
 
 
 // Modal --//confirmation//
 function sendQuantityToModal(idTo, idFrom){
     document.getElementById(idTo).innerText = document.getElementById(idFrom).value;
-}
+};
 
 function sendCostToModal(idTo, idFrom){
     document.getElementById(idTo).innerText = document.getElementById(idFrom).innerText;
-}
+};
 
 function confirmBooking(){
     sendQuantityToModal('modal-first-qty','first-class-qty');
     sendQuantityToModal('modal-economy-qty','economy-qty');
     sendCostToModal('modal-total','total');
-}
+};
