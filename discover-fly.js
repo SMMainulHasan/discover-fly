1e140d073ecf362252b6a16471f47892e3b945d8
SMMainulHasan <mainulhasan7070@gmail.com>
2021-01-30 15:12:58 +0600

    all ok

--- discover-fly.js
+++ discover-fly.js
@@ -1,15 +1,38 @@
 
-function plusMinusTicketQuantity(booleanIncreaseQuanitty, setType) {
+function plusMinusTicketQuantity(booleanIncreaseQuantity, setType, booleanFirstClass) {
     const ticketQuantity = document.getElementById(setType + '-qty');
     const numberOfTicket = parseInt(ticketQuantity.value);
     let ticketCount = numberOfTicket;
-    if (booleanIncreaseQuanitty == true) {
+    if (booleanIncreaseQuantity == true) {
         ticketCount = numberOfTicket + 1;
-    } if (booleanIncreaseQuanitty == false && ticketCount > 0) {
+    } if (booleanIncreaseQuantity == false && ticketCount > 0) {
         ticketCount = numberOfTicket - 1;
     }
     ticketQuantity.value = ticketCount;
+    totalCalculation(booleanFirstClass);
 }
-// function(){
 
-// }
+
+function totalCalculation(booleanFirstClass) {
+    const subtotal = document.getElementById('subtotal');
+
+    const firstClassQuantity = document.getElementById('first-class-qty');
+    const firstClassQuantityNum = parseInt(firstClassQuantity.value);
+    const economyQuantity = document.getElementById('economy-qty');
+    const economyQuantityNum = parseInt(economyQuantity.value);
+    let subtotalAmountFirst = firstClassQuantityNum * 150;
+    let subtotalAmountEconomy = economyQuantityNum * 100;
+    let subtotalCost = subtotalAmountFirst + subtotalAmountEconomy;
+    if (booleanFirstClass == true) {
+        subtotalAmountFirst = firstClassQuantityNum;
+    } if (booleanFirstClass == false) {
+        subtotalAmountEconomy = economyQuantityNum;
+    }
+    subtotal.innerText = ('$' + subtotalCost);
+
+    let vat = subtotalCost * 0.1;
+    document.getElementById('vat').innerText = ('$' + vat);
+
+    let total = subtotalCost + vat;
+    document.getElementById('total').innerText = ('$'+ total);
+}
